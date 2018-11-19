b016eade8fb448f64e5e77776a6e65c9a7d85849
Dian Wang <wangdian1007@gmail.com>
2018-11-18 20:31:30 -0500

    finished q_learning

--- scripts/q_learning.py
+++ scripts/q_learning.py
@@ -4,13 +4,34 @@ from collections import defaultdict
 
 import numpy as np
 
-from vrep_arm_toolkit.simulation import vrep
-from vrep_arm_toolkit.robots.ur5 import UR5
-from vrep_arm_toolkit.grippers.rdd import RDD
-import vrep_arm_toolkit.utils.vrep_utils as utils
-from vrep_arm_toolkit.utils import transformations
+from simple_task_env import SimpleTaskEnv
 
-VREP_BLOCKING = vrep.simx_opmode_blocking
+
+def greedyTieBreak(q_curr):
+    max_q = np.max(q_curr)
+    candidate = []
+    for i, q_value in enumerate(q_curr):
+        if q_value == max_q:
+            candidate.append(i)
+    return np.random.choice(candidate)
+
+
+def eGreedyActionSelection(q_curr, eps):
+    '''
+    Preforms epsilon greedy action selectoin based on the Q-values.
+
+    Args:
+        q_curr: A numpy array that contains the Q-values for each action for a state.
+        eps: The probability to select a random action. Float between 0 and 1.
+
+    Returns:
+        The selected action.
+    '''
+
+    if np.random.random() < eps:
+        return np.random.randint(len(q_curr))
+    else:
+        return greedyTieBreak(q_curr)
 
 
 class LinearSchedule(object):
@@ -33,47 +54,67 @@ class LinearSchedule(object):
         fraction = min(float(t) / self.schedule_timesteps, 1.0)
         return self.initial_p + fraction * (self.final_p - self.initial_p)
 
-# class QLearningAgent:
-#     def __init__(self, start_eps=1.0, final_eps=0.1, ):
-#         self.q_table = defaultdict(lambda: np.zeros(2))
-#
-#
-#
-#     def e_greedy(self, s):
 
+def q_learning(env, num_episodes, gamma=1.0, alpha=0.1,
+               start_eps=0.2, final_eps=0.1, annealing_steps=1000,
+               max_episode_steps=200):
+    '''
+    Q-learning algorithm.
 
-def reset(sim_client):
-    vrep.simxStopSimulation(sim_client, VREP_BLOCKING)
-    time.sleep(1)
-    vrep.simxStartSimulation(sim_client, VREP_BLOCKING)
-    time.sleep(1)
-    # Generate a cube
-    position = [-0.2, 0.9, 0.05]
-    orientation = [0, 0, 0]
-    # orientation = [0, 0, 0]
-    size = [0.1, 0.2, 0.05]
-    mass = 0.1
-    color = [255, 0, 0]
-    cube = utils.generateShape(sim_client, 'cube', 0, size, position, orientation, mass, color)
-    time.sleep(1)
+    Args:
+        - env: The environment to train the agent on
+        - num_episodes: The number of episodes to train the agent for
+        - gamma: The discount factor
+        - alpha: The stepsize
+        - start_eps: The initial epsilon value for e-greedy action selection
+        - final_eps: The final epsilon value for the e-greedy action selection
+        - annealing_steps: The number of steps to anneal epsilon over
+        - max_episode_steps: The maximum number of steps an episode can take
+
+    Returns: (Q_func, episode_rewards, episode_lengths)
+        - Q: Dictonary mapping state -> action values
+        - episode_rewards: Numpy array containing the reward of each episode during training
+        - episode_lengths: Numpy array containing the length of each episode during training
+    '''
+    Q = defaultdict(lambda: np.zeros(env.nA))
+    episode_rewards = np.zeros(num_episodes)
+    episode_lengths = np.zeros(num_episodes)
+
+    exploration = LinearSchedule(annealing_steps, start_eps, final_eps)
+    for episode in range(num_episodes):
+        print '------Episode {} / {}------'.format(episode, num_episodes)
+        s = round(env.reset(), 2)
+        r_total = 0
+        for step in range(max_episode_steps):
+            e = exploration.value(step)
+            a = eGreedyActionSelection(Q[s], e)
+            s_, r, done, info = env.step(a)
+            if not done:
+                s_ = round(s_, 2)
+                r_total += r
+                Q[s][a] += alpha * (r + gamma * np.max(Q[s_]) - Q[s][a])
+                s = s_
+            else:
+                r_total += r
+                Q[s][a] += alpha * (r - Q[s][a])
+            print 'step {}, {}, {}, {}, {}, {}'.format(step, s, a, s_, r, done)
+
+            if done or step == max_episode_steps - 1:
+                print '------Episode {} ended, total reward: {}, step: {}------'.format(episode, r_total, step)
+                episode_rewards[episode] = r_total
+                episode_lengths[episode] = step
+                break
+
+    return Q, episode_rewards, episode_lengths
 
 
 def main():
-    # Attempt to connect to simulator
-    sim_client = utils.connectToSimulation('127.0.0.1', 19997)
-
-    # Create UR5 and restart simulator
-    gripper = RDD(sim_client)
-    ur5 = UR5(sim_client, gripper)
-    vrep.simxStopSimulation(sim_client, VREP_BLOCKING)
-    time.sleep(1)
-    vrep.simxStartSimulation(sim_client, VREP_BLOCKING)
-    time.sleep(1)
-
-    reset(sim_client)
+    simple_task_env = SimpleTaskEnv()
+    q_table, rewards, lengths = q_learning(simple_task_env, 1000, gamma=0.9)
 
 
-
+if __name__ == '__main__':
+    main()
 
 
 
--- scripts/simple_task_env.py
+++ scripts/simple_task_env.py
@@ -20,6 +20,8 @@ class SimpleTaskEnv:
         self.nA = 2
 
         self.cube = None
+        self.cube_start_position = [-0.2, 0.9, 0.05]
+        self.cube_size = [0.1, 0.2, 0.04]
 
     def reset(self):
         vrep.simxStopSimulation(self.sim_client, VREP_BLOCKING)
@@ -27,33 +29,37 @@ class SimpleTaskEnv:
         vrep.simxStartSimulation(self.sim_client, VREP_BLOCKING)
         time.sleep(1)
         # Generate a cube
-        position = [-0.22, 0.9, 0.05]
+        position = self.cube_start_position
         orientation = [0, 0, 0]
         # orientation = [0, 0, 0]
-        size = [0.1, 0.2, 0.05]
-        mass = 0.2
+        size = self.cube_size
+        mass = 0.1
         color = [255, 0, 0]
         self.cube = utils.generateShape(self.sim_client, 'cube', 0, size, position, orientation, mass, color)
-        time.sleep(1)
+        # time.sleep(1)
 
-        # dy = 0.3 * np.random.random()
-        dy = 0.2
+        utils.setObjectPosition(self.sim_client, self.ur5.UR5_target, [-0.2, 0.6, 0.07])
+
+        dy = 0.3 * np.random.random()
+        # dy = 0.3
         current_pose = self.ur5.getEndEffectorPose()
         target_pose = current_pose.copy()
         target_pose[1, 3] += dy
 
         self.ur5.moveTo(target_pose)
-        time.sleep(0.5)
+        # time.sleep(0.5)
 
         self.rdd.openFinger(RDD.NARROW)
+
+        return self.rdd.getFingerPosition(RDD.NARROW)
 
     def step(self, a):
         current_pose = self.ur5.getEndEffectorPose()
         target_pose = current_pose.copy()
         if a == 0:
-            target_pose[1, 3] -= 0.01
+            target_pose[1, 3] -= 0.02
         else:
-            target_pose[1, 3] += 0.01
+            target_pose[1, 3] += 0.02
         utils.setObjectPosition(self.sim_client, self.ur5.UR5_target, target_pose[:3, 3])
         # utils.setObjectOrientation(self.sim_client, self.ur5.UR5_target, target_pose.flatten()[:-4])
         # self.ur5.moveTo(target_pose)
@@ -61,24 +67,33 @@ class SimpleTaskEnv:
 
         # arm is in wrong pose
         sim_ret, tip_position = utils.getObjectPosition(self.sim_client, self.ur5.gripper_tip)
-        if np.linalg.norm(tip_position - target_pose[:3, -1]) > 0.03:
-            print 'Wrong position, dist: ', np.linalg.norm(tip_position - target_pose[:3, -1])
-            return None, 0, True, None
+        # if np.linalg.norm(tip_position - target_pose[:3, -1]) > 0.05:
+        #     print 'Wrong position, dist: ', np.linalg.norm(tip_position - target_pose[:3, -1])
+        #     return None, -1, True, None
+        if tip_position[1] < 0.42 or tip_position[1] > 0.95:
+            print 'Wrong arm position: ', tip_position
+            return None, -1, True, None
         else:
             # cube is lifted
             sim_ret, cube_orientation = utils.getObjectOrientation(self.sim_client, self.cube)
-            print cube_orientation
             if cube_orientation[0] < -0.02:
                 return None, 1, True, None
-            # sim_ret, cube_pose = utils.getObjectPosition(self.sim_client, self.cube)
-            # while any(np.isnan(cube_pose)):
-            #     res, cube_pose = utils.getObjectPosition(self.sim_client, self.cube)
-            # if cube_pose[2] > 0.03:
-            #     return None, 1, True, None
+
+            # cube in wrong position
+            sim_ret, cube_position = utils.getObjectPosition(self.sim_client, self.cube)
+            while any(np.isnan(cube_position)):
+                res, cube_position = utils.getObjectPosition(self.sim_client, self.cube)
+            # if np.any(np.array(cube_position) < np.array(self.cube_start_position) - 0.5 * np.array(self.cube_size))\
+            #         or np.any(np.array(cube_position) > np.array(self.cube_start_position) + 0.5 * np.array(self.cube_size)):
+            if cube_position[0] < self.cube_start_position[0] - 0.5 * self.cube_size[0] or \
+                    cube_position[0] > self.cube_start_position[0] + 0.5 * self.cube_size[0] or\
+                    cube_position[1] < self.cube_start_position[1] - 0.5 * self.cube_size[1] or\
+                    cube_position[1] > self.cube_start_position[1] + 0.5 * self.cube_size[1]:
+                print 'Wrong cube position: ', cube_position
+                return None, 0, True, None
 
             # cube is not lifted
-            else:
-                return self.rdd.getFingerPosition(RDD.NARROW), 0, False, None
+            return self.rdd.getFingerPosition(RDD.NARROW), 0, False, None
 
 
 if __name__ == '__main__':
